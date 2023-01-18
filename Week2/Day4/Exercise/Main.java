78366a72dfb71f886da3f21518bd4da78ea81b35
Abdulrahman Mofarreh <amaz.alfaifi@gmail.com>
2023-01-18 17:10:12 +0300

    added main class to day4 exercise

---
+++ Week2/Day4/Exercise/Main.java
@@ -0,0 +1,17 @@
+package Exercise;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Animal> animals = new ArrayList<>();
+        animals.add(new Dog());
+        animals.add(new Cat());
+
+        animals.get(0).getName();
+        animals.get(1).getName();
+
+
+    }
+
+}
