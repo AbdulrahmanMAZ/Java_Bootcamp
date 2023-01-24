d6bded0a1e20e07fbbbf917e95c260296f270847
Abdulrahman Mofarreh <amaz.alfaifi@gmail.com>
2023-01-24 06:02:12 +0300

    Week 3 Day2 Assignment

---
+++ Week3/Day2/Homework-12/src/main/java/com/example/homework12/Homework12Application.java
@@ -0,0 +1,14 @@
+package com.example.homework12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class Homework12Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework12Application.class, args);
+    }
+
+}
