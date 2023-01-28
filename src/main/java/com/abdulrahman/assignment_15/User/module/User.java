0e35206c1e6198aee7cd2e3919fab7159dfe6be2
Abdulrahman Mofarreh <amaz.alfaifi@gmail.com>
2023-01-29 02:14:35 +0300

    Week 3 Day 4

---
+++ src/main/java/com/abdulrahman/assignment_15/User/module/User.java
@@ -0,0 +1,31 @@
+package com.abdulrahman.assignment_15.User.module;
+
+import jakarta.validation.constraints.*;
+import lombok.Data;
+
+@Data
+public class User {
+    @NotNull
+    @Size(min = 3)
+    private String id;
+    @NotEmpty
+    @Size(min = 5)
+    private String userName;
+    @NotEmpty
+    @Size(min = 6)
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$")
+    private String password;
+    @Email
+    @NotNull
+    @NotEmpty
+    private String email;
+    @NotEmpty
+    @NotNull
+    @Pattern(regexp = "^Admin||Customer$")
+    private String role;
+//    @Positive
+    @NotNull
+    @Pattern(regexp = "[0-9]+\\.{0,1}[0-9]{0,2}")
+    private String balance;
+
+}
